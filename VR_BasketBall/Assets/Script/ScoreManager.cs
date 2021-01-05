24e58e1ac2e07d4b40b4b098dc0f7d86698ff9d4
morganmark <32056855+morganmark@users.noreply.github.com>
2021-01-05 11:32:44 +0800

    20200105

---
+++ VR_BasketBall/Assets/Script/ScoreManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [Header("ScoreUI")]
+    public Text scoreText;
+    [Header("Score")]
+    public int scoreNumber;
+    [Header("當下投分")]
+    public int scoreInt=2;
+    [Header("進球音效")]
+    public AudioClip soundin;
+    private AudioSource aud;
+    private void Awake()
+    {
+        aud = GetComponent<AudioSource>();//抓取元件
+
+    }
+
+    private void OnTriggerEnter(Collider other)//進入碰撞範圍
+    {
+        //球進入籃框
+        if (other.tag=="BaseketBall")
+        {
+            //print(other.name);
+            AddScore();
+        }
+
+        //判定Player在3分區
+        if (other.transform.root.name=="Player")
+        {
+            print("InSide");
+            scoreInt = 3;
+        }
+    }
+    private void OnTriggerExit(Collider other)//離開碰撞範圍
+    {
+        if (other.transform.root.name == "Player")
+        {
+            print("OutSide");
+            scoreInt = 2;
+        }
+    }
+    private void AddScore()
+    {
+        scoreNumber += scoreInt;
+        scoreText.text = "分數:"+scoreNumber;
+        aud.PlayOneShot(soundin, 1.0f);//播放音效一次(音檔, 聲音大小)
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
